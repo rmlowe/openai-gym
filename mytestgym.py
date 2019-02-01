a70cfecd8895105a41e60577803fe2e2d9027382
Robert Lowe <robertmlowe@rmlowe.com>
2019-02-01 01:44:52 +0000

    Simple Neural Network Game

--- mytestgym.py
+++ mytestgym.py
@@ -1,18 +1,45 @@
+import tensorflow as tf
 import gym
+import numpy as np
 
+num_inputs = 4
+num_hidden = 4
+num_outputs = 1 # Prob to go left     1-left=right
+
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32,shape=[None,num_inputs])
+
+hidden_layer_one = tf.layers.dense(X,num_hidden,activation=tf.nn.relu,kernel_initializer=initializer)
+
+hidden_layer_two = tf.layers.dense(hidden_layer_one,num_hidden,activation=tf.nn.relu,kernel_initializer=initializer)
+
+output_layer = tf.layers.dense(hidden_layer_two,num_outputs,activation=tf.nn.sigmoid,kernel_initializer=initializer)
+
+probabilities = tf.concat(axis=1,values=[output_layer,1-output_layer])
+action = tf.multinomial(probabilities,num_samples=1)
+
+init = tf.global_variables_initializer()
+
+epi = 250
+step_limit = 500
 env = gym.make('CartPole-v0')
+avg_steps = []
 
-observation = env.reset()
+with tf.Session() as sess:
+    init.run()
 
-for t in range(1000):
+    for i_episode in range(epi):
+        obs = env.reset()
 
-    env.render()
+        for step in range(step_limit):
+            action_val = action.eval(feed_dict={X:obs.reshape(1,num_inputs)})
+            obs,reward,done,info = env.step(action_val[0][0]) # 0 or 1
 
-    cart_pos , cart_vel , pole_ang , ang_vel = observation
+            if done:
+                avg_steps.append(step)
+                print("DONE AFTER {} STEPS".format(step))
+                break
 
-    if pole_ang > 0:
-        action = 1
-    else:
-        action = 0
-
-    observation , reward, done, info = env.step(action)
+print("AFTER {} EPISODES, AVERAGE STEPS PER GAME WAS {} ".format(epi,np.mean(avg_steps)))
+env.close()
