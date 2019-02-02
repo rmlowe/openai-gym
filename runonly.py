78af7a5c7d024ab0dc3e12d3dbf60c2c5428831c
Robert Lowe <robertmlowe@rmlowe.com>
2019-02-02 13:49:53 +0000

    Policy Gradient Code Along Part Two

--- runonly.py
+++ runonly.py
@@ -2,6 +2,13 @@ import tensorflow as tf
 import gym
 import numpy as np
 
+# Same as 05 file, just no training, just loads the pre-trained model.
+
+
+##########################
+### VARIABLES ###########
+########################
+
 num_inputs = 4
 num_hidden = 4
 num_outputs = 1
@@ -10,24 +17,40 @@ learning_rate = 0.01
 
 initializer = tf.contrib.layers.variance_scaling_initializer()
 
-X = tf.placeholder(tf.float32,shape=[None,num_inputs])
 
-hidden_layer = tf.layers.dense(X,num_hidden,activation=tf.nn.elu,kernel_initializer=initializer)
+#################################
+### CREATING THE NETWORK #######
+###############################
 
-logits = tf.layers.dense(hidden_layer,num_outputs)
-outputs = tf.nn.sigmoid(logits)
+X = tf.placeholder(tf.float32, shape=[None, num_inputs])
 
-probabilities = tf.concat(axis=1,values=[outputs,1-outputs])
-action = tf.multinomial(probabilities,num_samples=1)
-
-y = 1.0 - tf.to_float(action)
-
+hidden_layer = tf.layers.dense(X, num_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
+logits = tf.layers.dense(hidden_layer, num_outputs)
+outputs = tf.nn.sigmoid(logits)  # probability of action 0 (left)
 
 
-cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=logits)
+probabilties = tf.concat(axis=1, values=[outputs, 1 - outputs])
+action = tf.multinomial( probabilties, num_samples=1)
+
+# Convert from Tensor to number for network training
+y = 1. - tf.to_float(action)
+
+########################################
+### LOSS FUNCTION AND OPTIMIZATION ####
+######################################
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
 optimizer = tf.train.AdamOptimizer(learning_rate)
 
+# https://stackoverflow.com/questions/41954198/optimizer-compute-gradients-how-the-gradients-are-calculated-programatically
+# https://www.tensorflow.org/api_docs/python/tf/train/AdamOptimizer
+
+
+################################
+#### GRADIENTS ################
+##############################
 gradients_and_variables = optimizer.compute_gradients(cross_entropy)
+
+
 
 gradients = []
 gradient_placeholders = []
@@ -39,11 +62,16 @@ for gradient, variable in gradients_and_variables:
     gradient_placeholders.append(gradient_placeholder)
     grads_and_vars_feed.append((gradient_placeholder, variable))
 
-training_op = optimizer.apply_gradients(grads_and_vars_feed)
 
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
 
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
+
+######################################
+#### REWARD FUNCTIONs ################
+####################################
+# CHECK OUT: https://medium.com/@awjuliani/super-simple-reinforcement-learning-tutorial-part-2-ded33892c724
 
 def helper_discount_rewards(rewards, discount_rate):
     '''
@@ -69,62 +97,6 @@ def discount_and_normalize_rewards(all_rewards, discount_rate):
     reward_mean = flat_rewards.mean()
     reward_std = flat_rewards.std()
     return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
-
-env = gym.make('CartPole-v0')
-
-num_game_rounds = 10
-max_game_steps = 1000
-num_iterations = 750
-discount_rate = 0.9
-
-with tf.Session() as sess:
-
-    sess.run(init)
-
-
-    for iteration in range(num_iterations):
-        print('On iteration: {}'.format(iteration))
-
-        all_rewards = []
-        all_gradients = []
-
-        for game in range(num_game_rounds):
-
-            current_rewards = []
-            current_gradients = []
-
-            observations = env.reset()
-
-            for step in range(max_game_steps):
-
-                action_val , gradients_val = sess.run([action,gradients], feed_dict = {X:observations.reshape(1,num_inputs)})
-
-                observations, reward, done, info = env.step(action_val[0][0])
-
-                current_rewards.append(reward)
-                current_gradients.append(gradients_val)
-
-                if done:
-                    break
-
-            all_rewards.append(current_rewards)
-            all_gradients.append(current_gradients)
-
-        all_rewards = discount_and_normalize_rewards(all_rewards,discount_rate)
-        feed_dict = {}
-
-        for var_index, gradient_placeholder in enumerate(gradient_placeholders):
-            mean_gradients = np.mean([reward * all_gradients[game_index][step][var_index]
-                                      for game_index, rewards in enumerate(all_rewards)
-                                          for step, reward in enumerate(rewards)], axis=0)
-            feed_dict[gradient_placeholder] = mean_gradients
-
-        sess.run(training_op,feed_dict=feed_dict)
-
-        print('SAVING GRAPH AND SESSION')
-
-        meta_graph_def = tf.train.export_meta_graph(filename='models/my-750-step-model.meta')
-        saver.save(sess,'models/my-750-step-model')
 
 #############################################
 ### RUN TRAINED MODEL ON ENVIRONMENT #######
